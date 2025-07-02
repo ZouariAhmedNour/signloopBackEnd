ed8f26fa21085138e9e8da8d6d021886204548c7
ZouariAhmedNour <zouariahmednour@gmail.com>
2025-07-02 01:26:39 +0100

    First commit just initializing the project

---
+++ src/main/java/com/signloop/signloop/SignloopApplication.java
@@ -0,0 +1,13 @@
+package com.signloop.signloop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignloopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignloopApplication.class, args);
+	}
+
+}
